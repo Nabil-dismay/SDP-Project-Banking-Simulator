4e62152d883804debbd475d7f0f61f3ea76fad3e
Enter the Ocean <enterthecoldocean@gmail.com>
2026-04-29 17:13:58 +0200

    Added test cases for banking functions

---
+++ FinalRepotTestcases.js
@@ -0,0 +1,89 @@
+account = {
+  owner: "Alice",
+  balance: 1000,
+  history: [
+    { type: "DEPOSIT", amount: 500 },
+    { type: "WITHDRAW", amount: 200 },
+    { type: "TRANSFER", amount: 300 }
+  ]
+};
+
+account = {
+  owner: "Bob",
+  balance: 2000,
+  history: [
+    { type: "DEPOSIT", amount: 300 },
+    { type: "DEPOSIT", amount: 700 },
+    { type: "WITHDRAW", amount: 100 },
+    { type: "WITHDRAW", amount: 200 }
+  ]
+};
+
+account = {
+  owner: "Charlie",
+  balance: 0,
+  history: []
+};
+
+account = {
+  owner: "Dana",
+  balance: 500,
+  history: [
+    { type: "DEPOSIT", amount: 200 },
+    { type: "BONUS", amount: 100 }
+  ]
+};
+
+account = {
+  owner: "Eve",
+  balance: 300,
+  history: [
+    { type: "DEPOSIT" }
+  ]
+};
+
+account = {
+  owner: "Frank",
+  balance: 100,
+  history: [
+    { type: "DEPOSIT", amount: -500 },
+    { type: "WITHDRAW", amount: -100 }
+  ]
+};
+
+account = {
+  owner: "Grace",
+  balance: 800,
+  history: [
+    { type: "deposit", amount: 100 },
+    { type: "WITHDRAW", amount: 50 }
+  ]
+};
+
+account = {
+  owner: "Henry",
+  balance: 1_000_000,
+  history: [
+    { type: "DEPOSIT", amount: 900000 },
+    { type: "TRANSFER", amount: 800000 }
+  ]
+};
+
+account = {
+  owner: "Ivy",
+  balance: 100,
+  history: null
+};
+
+account = undefined;
+
+history: [
+  { type: "TRANSFER", amount: 300 },
+  { type: "DEPOSIT", amount: 500 },
+  { type: "WITHDRAW", amount: 200 }
+]
+
+history: [
+  { type: "DEPOSIT", amount: 0 }
+]
+
